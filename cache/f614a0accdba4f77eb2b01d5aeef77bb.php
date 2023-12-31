413269ab7bb5ce1b47a333046f00ed84001c115c
Anon <github@christensen.wtf>
2023-12-30 20:21:59 -0800

    update db location

--- cache/f614a0accdba4f77eb2b01d5aeef77bb.php
+++ cache/f614a0accdba4f77eb2b01d5aeef77bb.php
@@ -16,7 +16,7 @@
                     <h4 class="mb-0">toddchristensen.net</h4>
                 </a>
                 <p class="mt-0">This site. My portfolio site. There's been many variations over the years. I've decided
-                    to keep it pretty vanilla this time-- plain php, tailwind, &amp; htmx.</p>
+                    to keep it pretty vanilla this time-- plain php, tailwind, alpine, &amp; htmx.</p>
             </li>
             <li>
                 <a href="https://adopt.com.ph">
@@ -28,7 +28,8 @@
                 <a href="https://christyvault.com">
                     <h4 class="mb-0">Christy Vault</h4>
                 </a>
-                <p class="mt-0">Company website (static). I now work here as Operations Manager.</p>
+                <p class="mt-0">Company website (static). I now work here as Operations Manager. Current live site was
+                    made with Next.js. New site will be made with Astro js, htmx and alpine</p>
             </li>
         </ul>
         <div class="flex flex-row items-center gap-4">
--- src/DB.php
+++ src/DB.php
@@ -12,7 +12,7 @@ class DB
     public function __construct()
     {
         try {
-            $this->connection = new PDO('sqlite:../todd.db'); // Adjust the path as necessary
+            $this->connection = new PDO('sqlite:../storage/todd.db'); // Adjust the path as necessary
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             // Handle the error appropriately
