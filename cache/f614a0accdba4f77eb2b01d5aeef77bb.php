2f224ca55b9fbc4db19a91ae601cb4e90c1ddb9a
Anon <github@christensen.wtf>
2024-01-02 09:51:17 -0800

    h4 to h3 for project list

--- cache/f614a0accdba4f77eb2b01d5aeef77bb.php
+++ cache/f614a0accdba4f77eb2b01d5aeef77bb.php
@@ -4,20 +4,20 @@
         <ul class="latest-list">
             <li>
                 <a href="https://www.toddchristensen.net">
-                    <h4>toddchristensen.net</h4>
+                    <h3>toddchristensen.net</h3>
                 </a>
                 <p class="mt-0">This site. My portfolio site. There's been many variations over the years. I've decided
                     to keep it pretty vanilla this time-- plain php, tailwind, alpine, &amp; htmx.</p>
             </li>
             <li>
                 <a href="https://adopt.com.ph">
-                    <h4>adoptapet.com.ph</h4>
+                    <h3>adoptapet.com.ph</h3>
                 </a>
                 <p class="mt-0">pet adoption platform for Philippines.</p>
             </li>
             <li>
                 <a href="https://christyvault.com">
-                    <h4>Christy Vault</h4>
+                    <h3>Christy Vault</h3>
                 </a>
                 <p class="mt-0">Company website (static). I now work here as Operations Manager. Current live site was
                     made with Next.js. New site will be made with Astro js, htmx and alpine</p>
