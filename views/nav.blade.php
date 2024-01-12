3e69a2c65a8db25a086d458b20306a7ed372ede6
Anon <github@christensen.wtf>
2024-01-12 13:37:51 -0800

    underline nav links

--- views/nav.blade.php
+++ views/nav.blade.php
@@ -1,8 +1,10 @@
  <nav hx-boost="true" class="flex flex-col flex-wrap w-full gap-4 header-nav">
-     <a class="nav-link" hx-boost="true" href="/" hx-get="/" hx-push-url="/" hx-target="main">
+     <a class="transition-all duration-300 ease-linear hover:underline nav-link" hx-boost="true" href="/"
+         hx-get="/" hx-push-url="/" hx-target="main">
          home
      </a>
-     <a class="nav-link" hx-boost="true" href="/prius" hx-get="/prius" hx-push-url="/prius" hx-target="main">
+     <a class="transition-all duration-300 ease-linear hover:underline nav-link" hx-boost="true" href="/prius"
+         hx-get="/prius" hx-push-url="/prius" hx-target="main">
          prius
      </a>
      {{-- <a href="/projects" hx-get="/projects" hx-push-url="/projects" hx-target="main">some projects</a> --}}
