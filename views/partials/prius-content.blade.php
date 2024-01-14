37931bbc31843a3af1203cdf68863089edd7945f
Anon <github@christensen.wtf>
2024-01-13 20:24:32 -0800

    prius page content

--- views/partials/prius-content.blade.php
+++ views/partials/prius-content.blade.php
@@ -2,5 +2,6 @@
     parting out my 2008 Toyota Prius. here's what I have..
     <ul>
         <li>Everything except for a working engine.</li>
+        <li>Barely used catalytic converter.</li>
     </ul>
 </div>
