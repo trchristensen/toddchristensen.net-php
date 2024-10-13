a5d9ef1c67ab54c19c6cd9016553ab342852d70f
Todd <github@christensen.wtf>
2024-10-13 13:40:03 -0700

    remove contact link from nav

--- views/nav.blade.php
+++ views/nav.blade.php
@@ -12,6 +12,6 @@
      {{-- <a class="nav-link" href="/about" hx-get="/about" hx-push-url="/about" hx-target="main">more
          about me</a> --}}
      {{-- <a class="nav-link" href="/guestbook" hx-get="/guestbook" hx-push-url="/guestbook" hx-target="main">guestbook</a> --}}
-     <a href="/contact" hx-get="/contact" hx-push-url="/contact" hx-target="main">hit
-         me up</a>
+     {{-- <a href="/contact" hx-get="/contact" hx-push-url="/contact" hx-target="main">hit
+         me up</a> --}}
  </nav>
