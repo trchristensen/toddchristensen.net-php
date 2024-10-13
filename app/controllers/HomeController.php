44e4d0cfd3cc6e10d366703793fb3c24f61cbbd1
Todd <github@christensen.wtf>
2024-10-13 09:42:53 -0700

    fix controller logic

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,23 +2,36 @@
 
 namespace App\Controllers;
 
+use App\Router;
 use App\Controller;
-
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return $this->view('home', ['title' => 'Home | Todd Christensen']);
+        return Router::isHtmxAjaxRequest() ?
+            $this->view('partials.home-content')
+            : $this->view('home', ['title' => 'Home | Todd Christensen']);
     }
 
     public function about()
     {
-        return $this->view('about', ['title' => 'About | Todd Christensen']);
+        return Router::isHtmxAjaxRequest() ?
+            $this->view('partials.about-content')
+            : $this->view('about', ['title' => 'About | Todd Christensen']);
     }
 
     public function contact()
     {
-        return $this->view('contact', ['title' => 'Contact | Todd Christensen']);
+        return Router::isHtmxAjaxRequest() ?
+            $this->view('partials.contact-content')
+            : $this->view('contact', ['title' => 'Contact | Todd Christensen']);
+    }
+
+    public function prius()
+    {
+        return Router::isHtmxAjaxRequest() ?
+            $this->view('partials.prius-content')
+            : $this->view('prius');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,12 @@ use App\Controllers\GuestbookController;
 use App\Router;
 
 Router::get('/', [HomeController::class, 'index']);
+Router::get('/prius', [HomeController::class, 'prius']);
 
-Router::get('/about', [HomeController::class, 'about']);
-Router::get('/contact', [HomeController::class, 'contact']);
+// Router::get('/about', [HomeController::class, 'about']);
+// Router::get('/contact', [HomeController::class, 'contact']);
 
-Router::get('/guestbook', [GuestbookController::class, 'index']);
-Router::post('/guestbook', [GuestbookController::class, 'store']);
+// Router::get('/guestbook', [GuestbookController::class, 'index']);
+// Router::post('/guestbook', [GuestbookController::class, 'store']);
 
 // Add other routes as needed
